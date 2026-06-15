ad258ad004574701d2aa1833d322814153159d9f
Tanya Samastroyenka <tanyasamastr@gmail.com>
2026-06-15 22:44:25 +0300

    Initial Commit

---
+++ aiGeneratorTestTask/aiGeneratorTestTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  aiGeneratorTestTaskApp.swift
+//  aiGeneratorTestTask
+//
+//  Created by Sanya Tamostroenko on 15.06.26.
+//
+
+import SwiftUI
+
+@main
+struct aiGeneratorTestTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
