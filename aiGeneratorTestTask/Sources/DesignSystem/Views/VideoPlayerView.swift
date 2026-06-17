50d4ad5fd61473f3296e22e3817917e5850e4946
Tanya Samastroyenka <tanyasamastr@gmail.com>
2026-06-17 19:17:44 +0300

    videoPlayerView

--- aiGeneratorTestTask/Sources/DesignSystem/Views/VideoPlayerView.swift
+++ aiGeneratorTestTask/Sources/DesignSystem/Views/VideoPlayerView.swift
@@ -9,16 +9,44 @@ import AVKit
 
 struct VideoPlayerView: UIViewControllerRepresentable {
     
-    let player: AVPlayer
-
+    let url: URL
+    
+    @Environment(\.dismiss) private var dismiss
+    
     func makeUIViewController(context: Context) -> AVPlayerViewController {
+        let player = AVPlayer(url: url)
         let vc = AVPlayerViewController()
         vc.player = player
-        vc.showsPlaybackControls = true
         vc.videoGravity = .resizeAspect
-        
+        vc.delegate = context.coordinator
+        player.play()
         return vc
     }
-
+    
     func updateUIViewController(_ vc: AVPlayerViewController, context: Context) {}
+    
+    func makeCoordinator() -> Coordinator { Coordinator(dismiss: dismiss) }
+    
+    final class Coordinator: NSObject, AVPlayerViewControllerDelegate {
+        
+        let dismiss: DismissAction
+        
+        init(dismiss: DismissAction) {
+            self.dismiss = dismiss
+        }
+        
+        func playerViewController(
+            _ playerViewController: AVPlayerViewController,
+            willBeginFullScreenPresentationWithAnimationCoordinator coordinator: UIViewControllerTransitionCoordinator
+        ) {}
+        
+        func playerViewController(
+            _ playerViewController: AVPlayerViewController,
+            willEndFullScreenPresentationWithAnimationCoordinator coordinator: UIViewControllerTransitionCoordinator
+        ) {
+            coordinator.animate(alongsideTransition: nil) { _ in
+                self.dismiss()
+            }
+        }
+    }
 }
