7a5b980ed2e2c82daead564d96d3555d95dd13d4
Tanya Samastroyenka <tanyasamastr@gmail.com>
2026-06-19 01:51:15 +0300

    app constants

---
+++ aiGeneratorTestTask/Sources/AppConstants/AppConstants.swift
@@ -0,0 +1,18 @@
+//
+//  AppConstants.swift
+//  aiGeneratorTestTask
+//
+
+import Foundation
+
+enum AppConstants {
+    enum PaywallLink {
+        static let privacyUrl = "https://docs.google.com/document/d/1k7kPfCLckfuu75oC67lr0CApYB6GkUvsVLOQkzImCRo/edit?tab=t.0#heading=h.6ljhnil22eo2"
+        static let termsUrl = "https://docs.google.com/document/d/1k7kPfCLckfuu75oC67lr0CApYB6GkUvsVLOQkzImCRo/edit?tab=t.0#heading=h.6ljhnil22eo2"
+    }
+    
+    enum Apphud {
+        static let key = "app_FmCjFTwjWpcLSafxT8vCDeVffJyfFS"
+        static let paywallID = "main"
+    }
+}
