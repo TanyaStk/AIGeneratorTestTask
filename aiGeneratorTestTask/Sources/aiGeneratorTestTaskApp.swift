edebf52b0b08a259cb85f8281e08578d3a8cc39a
Tanya Samastroyenka <tanyasamastr@gmail.com>
2026-06-18 18:15:08 +0300

    fetching user id from apphud

--- aiGeneratorTestTask/Sources/aiGeneratorTestTaskApp.swift
+++ aiGeneratorTestTask/Sources/aiGeneratorTestTaskApp.swift
@@ -4,14 +4,28 @@
 //
 
 import SwiftUI
+import ApphudSDK
 
 @main
 struct aiGeneratorTestTaskApp: App {
+    
+    @AppStorage("userID")
+    private var userID: String = ""
+    
+    init() {
+        setupApphud()
+    }
+    
     var body: some Scene {
         WindowGroup {
             MainScreenView()
                 .embedRouter()
                 .colorScheme(.dark)
         }
+    }
+    
+    private func setupApphud() {
+        Apphud.start(apiKey: "app_FmCjFTwjWpcLSafxT8vCDeVffJyfFS")
+        userID = Apphud.userID()
     }
 }
