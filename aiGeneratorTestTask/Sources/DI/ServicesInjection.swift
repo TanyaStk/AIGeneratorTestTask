3c64877a95ea651cd1646f211e3a77deb88822bb
Tanya Samastroyenka <tanyasamastr@gmail.com>
2026-06-19 19:46:32 +0300

    chat history service injection

--- aiGeneratorTestTask/Sources/DI/ServicesInjection.swift
+++ aiGeneratorTestTask/Sources/DI/ServicesInjection.swift
@@ -12,17 +12,12 @@ extension InjectedValues {
         get { Self[VideoHistoryRepositoryKey.self] }
         set { Self[VideoHistoryRepositoryKey.self] = newValue }
     }
-
-    var chatHistoryRepository: ChatHistoryRepository {
-        get { Self[ChatHistoryRepositoryKey.self] }
-        set { Self[ChatHistoryRepositoryKey.self] = newValue }
-    }
-
+    
     var videoFileManager: VideoFileManager {
         get { Self[VideoFileManagerKey.self] }
         set { Self[VideoFileManagerKey.self] = newValue }
     }
-
+    
     var videoGenerationService: VideoGenerationService {
         get { Self[VideoGenerationServiceKey.self] }
         set { Self[VideoGenerationServiceKey.self] = newValue }
@@ -43,6 +38,11 @@ extension InjectedValues {
         set { Self[ChatServiceKey.self] = newValue }
     }
     
+    var chatHistoryService: ChatHistoryServiceProvider {
+        get { Self[ChatHistoryServiceKey.self] }
+        set { Self[ChatHistoryServiceKey.self] = newValue }
+    }
+    
     var apphudService: ApphudServiceType {
         get { Self[ApphudServiceKey.self] }
         set { Self[ApphudServiceKey.self] = newValue }
@@ -53,10 +53,7 @@ extension InjectedValues {
 
 extension InjectedValues {
     static func setupForPreviews() {
-        let context = PersistenceController(inMemory: true).context
-        
         InjectedValues[\.videoHistoryRepository] = MockVideoHistoryRepository()
-        InjectedValues[\.chatHistoryRepository] = ChatHistoryRepository(context: context)
         InjectedValues[\.videoGenerationService] = MockVideoGenerationService(successRate: 1.0)
         InjectedValues[\.videoTemplateProvider] = MockVideoTemplateService()
         InjectedValues[\.chatService] = MockChatService()
@@ -67,10 +64,6 @@ extension InjectedValues {
 
 private struct VideoHistoryRepositoryKey: InjectionKey {
     static var currentValue: VideoHistoryRepositoryProtocol = VideoHistoryRepository()
-}
-
-private struct ChatHistoryRepositoryKey: InjectionKey {
-    static var currentValue: ChatHistoryRepository = ChatHistoryRepository()
 }
 
 private struct VideoFileManagerKey: InjectionKey {
@@ -95,4 +88,10 @@ private struct ChatServiceKey: InjectionKey {
 
 private struct ApphudServiceKey: InjectionKey {
     static var currentValue: ApphudServiceType = ApphudService()
+}
+
+private struct ChatHistoryServiceKey: InjectionKey {
+    static var currentValue: ChatHistoryServiceProvider = ChatHistoryService(
+        network: InjectedValues[\.networkService]
+    )
 }
